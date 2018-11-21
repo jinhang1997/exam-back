f12d61fa14847f6eaee3fe7ab26547e832b5c5cd
Jin Hang <jinhang@nuaa.edu.cn>
2018-11-21 20:50:19 +0800

    added basis logic to getting paper list

--- backend/models.py
+++ backend/models.py
@@ -9,3 +9,11 @@ class UserList(models.Model):
   username = models.CharField(max_length = 20, unique = True)
   password = models.CharField(max_length = 20)
   usertype = models.CharField(max_length = 20)
+
+class Paper(models.Model):
+  pid = models.CharField(max_length = 20)
+  pname = models.CharField(max_length = 20)
+  teaname = models.CharField(max_length = 20)
+  stulist = models.CharField(max_length = 10240)
+  prolist = models.CharField(max_length = 10240)
+  penabled = models.CharField(max_length = 20)
--- backend/test_manage.py
+++ backend/test_manage.py
@@ -8,8 +8,21 @@ from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from backend.models import UserList
+from backend.models import Paper
 import json
 import os
+
+class claPaper:
+  def __init__(self, pid, pname, teaname, penabled, stulist, prolist):
+    self.pid = pid
+    self.pname = pname
+    self.teaname = teaname
+    self.penabled = penabled
+    self.stulist = stulist
+    self.prolist = prolist
+  def __repr__(self):
+    return repr((self.pid, self.pname, self.teaname,
+    self.penabled, self.stulist, self.prolist))
 
 def get_avaliable(request):
   ret = {'code': 200, 'list': 'test list of [%s]' % (request.session['login_name']) }
@@ -20,5 +33,18 @@ def get_history(request):
   return HttpResponse(json.dumps(ret), content_type="application/json")
 
 def get_tea_testlist(request):
-  ret = {'code': 200, 'list': 'test list of [%s]' % (request.session['login_name']) }
+  tname = request.session['login_name']
+  print(tname)
+  papers = Paper.objects.all()#.filter(teaname = tname)
+  print(papers)
+  plist = []
+  for var in papers:
+    plist.append(claPaper(var.pid, var.pname, var.teaname,
+      var.penabled, var.stulist, var.prolist))
+  print(plist)
+  jsonarr = json.dumps(plist, default=lambda o: o.__dict__, sort_keys=True)
+  print(jsonarr)
+  loadarr = json.loads(jsonarr)
+  print(loadarr)
+  ret = {'code': 200, 'list': loadarr }
   return HttpResponse(json.dumps(ret), content_type="application/json")
